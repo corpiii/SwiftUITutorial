ad9f71bf2ee9e9a2cb5ed9b655b729b6a0620c85
corpi <lj845854@gmail.com>
2024-02-22 18:04:17 +0900

    Initial Commit

---
+++ SwiftUITutorial/SwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialApp.swift
+//  SwiftUITutorial
+//
+//  Created by 이정민 on 2/22/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
